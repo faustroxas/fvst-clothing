018b8bcc7924cd9c2fc87d76706885d56e6365e0
Faust Roxas <faustroxas@Fausts-MacBook-Pro.local>
2021-03-05 13:50:40 +0800

    Finished homepage display

---
+++ src/pages/homepage/homepage-component.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './homepage-style.scss';
+import { MenuDirectory } from '../../components/menu-directory/menu-directory-component';
+
+export const Homepage = () => (
+    <div className='homepage'>
+        <MenuDirectory />
+    </div>
+)
