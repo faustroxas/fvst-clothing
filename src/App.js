2b19f11f5a44bbdd63f133719a4fce2924d637d5
Faust Roxas <faustroxas@Fausts-MacBook-Pro.local>
2021-03-09 23:25:26 +0800

    Updated menu items to navigate to hats page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import './App.css';
+import { Switch, Route } from 'react-router-dom';
 import { Homepage } from './pages/homepage/homepage-component';
+import { HatsPage } from './pages/hatspage/hatspage-component';
+
+import './App.css';
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route exact path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/hatspage/hatspage-component.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const HatsPage = () => (
+    <div>
+      <h1>HATS PAGE!</h1>
+    </div>
+)
